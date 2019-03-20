f68b6a7fa794c45809a91a7814be0447bf58c4df
Khalil Daoulat <48126333+Vvoox@users.noreply.github.com>
2019-03-20 14:25:00 +0000

    Update Email_sender.py

--- Email_sender.py
+++ Email_sender.py
@@ -1,28 +1,26 @@
 #Hey everybody , this code it just for facilitate task of sending msg to many emails here its just for gmail . but you can change the host and port for other mails platformes .
 #If you've any problems or need some support please contact me , on Github or any other platforme . Good luck
-#Still not finish this version.
 
 import smtplib
-#from email.MIMEMultipart import MIMEMultipart
-#from email.MIMEText import MIMEText
+from email.mime.text import MIMEText
 
-f = open("Your emails list ","r")
+f = open("your_email_list.txt","r")
 myList = []
 for line in f:
         myList.append(line)
 
 print(myList)
 
-sender = "your mail"
+sender = "You email"
 receivers = myList
 server = smtplib.SMTP('smtp.gmail.com',587)
 server.starttls()
-msg = MIMEText('Your main text')
+msg = MIMEText('Your message')
 msg['Subject'] = "subject line"
 msg['From'] = sender
 msg['To'] = ", ".join(receivers)
-server.login('your mail','your mail password')
-server.sendmail(sender, recipients, msg.as_string())
+server.login('Your email ','Your password')
+server.sendmail(sender, receivers, msg.as_string())
 server.quit()
 
 #Notice : Do not show your code after editing, cause will contain sensitive informations .
